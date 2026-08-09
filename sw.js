e69eeabccddf76f7ffd1e54c8397e503599207ed
luismariarios-collab <luismariarios@gmail.com>
2026-08-08 23:30:21 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'db-analisis-v1';
+const CACHE = 'db-analisis-v2';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
